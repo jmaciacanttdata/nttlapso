02731ebeaedc31d0326dc9bb102e49643ffec170
jmaciacanttdata <jmaciacanttdata@gmail.com>
2023-04-19 08:39:59 +0200

    Agregada autenticación de usuario mediante JWT.
    Agregada autenticación de usuario mediante JWT.

---
+++ NTTLapso/Models/Login/LoginDataResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace NTTLapso.Models.Login
+{
+    public class LoginDataResponse
+    {
+        public LoginResponse Data { get; set; }
+        public DateTime DateLogin { get; set; }
+        public DateTime DateLoginExpires { get; set; }
+        public string Token { get; set; }
+    }
+}
