56bf0508de001d2e49a097cbc8da2f077570f044
jmaciacanttdata <jmaciacanttdata@gmail.com>
2023-04-25 09:21:35 +0200

    Creación del controlador, servicio y repositorio para masters.

---
+++ NTTLapso/Repository/MastersRepository.cs
@@ -0,0 +1,16 @@
+﻿using Dapper;
+using MySqlConnector;
+using NTTLapso.Models.Login;
+
+namespace NTTLapso.Repository
+{
+    public class MastersRepository
+    {
+        private static string connectionString = "Server=POAPMYSQL143.dns-servicio.com;User ID=nttlapso;Password=kP0?8u50a;Database=8649628_nttlapso";
+        private MySqlConnection conn;
+
+        public MastersRepository() { 
+            conn = new MySqlConnection(connectionString);
+        }
+    }
+}
---
+++ NTTLapso/Service/MastersService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using NTTLapso.Models.Login;
+using NTTLapso.Repository;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace NTTLapso.Service
+{
+    public class MastersService
+    {
+        private MastersRepository _repo = new MastersRepository();
+        public MastersService() { }
+
+        
+    }
+}
