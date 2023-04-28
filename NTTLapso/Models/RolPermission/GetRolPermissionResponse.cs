5ada6b78b510a9908c874e1ed897a58936125445
svenswersnttdata <sveswers@emeal.nttdata.com>
2023-04-28 10:50:42 +0200

    Creacion metodos CRUD sobre tabla rol permissions

---
+++ NTTLapso/Models/RolPermission/GetRolPermissionResponse.cs
@@ -0,0 +1,11 @@
+﻿using NTTLapso.Models.General;
+
+namespace NTTLapso.Models.RolPermission
+{
+    public class GetRolPermissionResponse
+    {
+        public List<RolPermissionDataResponse> Data { get; set; } = new List<RolPermissionDataResponse>();
+
+        public ErrorResponse Error { get; set; } = new ErrorResponse();
+    }
+}
--- NTTLapso/Service/MastersService.cs
+++ NTTLapso/Service/MastersService.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using NTTLapso.Models.General;
 using NTTLapso.Models.Login;
+using NTTLapso.Models.RolPermission;
 using NTTLapso.Repository;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -12,6 +15,28 @@ namespace NTTLapso.Service
         private MastersRepository _repo = new MastersRepository();
         public MastersService() { }
 
-        
+        // Register a new rol with it's permissions.
+        internal async Task<ErrorResponse> SetRolPermission(SetRolPermissionRequest request)
+        {
+            return await _repo.SetRolPermission(request);
+        }
+
+        // Get a list of rols with it's permissions.
+        public async Task<GetRolPermissionResponse> GetRolsPermissionList()
+        {
+            return await _repo.GetRolsPermissionList();
+        }
+
+        // Update rol permissions.
+        public async Task<ErrorResponse> UpdateRolPermission(SetRolPermissionRequest request)
+        {
+            return await _repo.UpdateRolPermission(request);
+        }
+
+        // Delete rol permissions.
+        internal async Task<ErrorResponse> DeleteRolPermissions(int rolId)
+        {
+            return await _repo.DeleteRolPermissions(rolId);
+        }
     }
 }
