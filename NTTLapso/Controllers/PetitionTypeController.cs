ee81c7fd1aaf48a6048214ad26d58e7a8106cf58
svenswersnttdata <sveswers@emeal.nttdata.com>
2023-05-03 11:42:53 +0200

    Creacion endpoints CRUD sobre tabla BD 'petition_type'

--- NTTLapso/Controllers/PetitionTypeController.cs
+++ NTTLapso/Controllers/PetitionTypeController.cs
@@ -1,39 +1,33 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using NTTLapso.Models.Category;
+﻿using Microsoft.AspNetCore.Mvc;
 using NTTLapso.Models.General;
-using NTTLapso.Models.Login;
 using NTTLapso.Models.Rol;
 using NTTLapso.Service;
-using System.IdentityModel.Tokens.Jwt;
-using System.Net;
-using System.Security.Claims;
-using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using NTTLapso.Models.PetitionType;
 
 namespace NTTLapso.Controllers
 {
     [ApiController]
-    [Route("Rol")]
-    public class RolController : ControllerBase
+    [Route("PetitionType")]
+    public class PetitionTypeController : ControllerBase
     {
         private readonly IConfiguration _config;
-        private readonly ILogger<RolController> _logger;
-        private RolService _service = new RolService();
-        public RolController(ILogger<RolController> logger, IConfiguration config)
+        private readonly ILogger<PetitionTypeController> _logger;
+        private PetitionTypeService _service = new PetitionTypeService();
+        public PetitionTypeController(ILogger<PetitionTypeController> logger, IConfiguration config)
         {
             _logger = logger;
             _config = config;
         }
 
-
+        // Get petition type list.
         [HttpPost]
         [Route("List")]
         [AllowAnonymous]
-        public async Task<ListRolResponse> List(IdValue? request) {
-            ListRolResponse response = new ListRolResponse();
-            List<IdValue> responseList = new List<IdValue>();
+        public async Task<ListPetitionTypeResponse> List(PetitionTypeRequest? request)
+        {
+            ListPetitionTypeResponse response = new ListPetitionTypeResponse();
+            List<PetitionTypeDataResponse> responseList = new List<PetitionTypeDataResponse>();
             try
             {
                 responseList = await _service.List(request);
@@ -51,18 +45,20 @@ namespace NTTLapso.Controllers
             return response;
         }
 
+        // Create new petition type.
         [HttpPost]
         [Route("Create")]
         [AllowAnonymous]
-        public async Task<RolResponse> Create(string value)
+        public async Task<PetitionTypeResponse> Create(string value, bool selectable)
         {
-            RolResponse response = new RolResponse();
+            PetitionTypeResponse response = new PetitionTypeResponse();
 
             try
             {
-                await _service.Create(value);
+                await _service.Create(value, selectable);
                 response.IsSuccess = true;
-            }catch(Exception ex)
+            }
+            catch (Exception ex)
             {
                 Error _error = new Error(ex);
                 response.IsSuccess = false;
@@ -73,12 +69,13 @@ namespace NTTLapso.Controllers
             return response;
         }
 
+        // Edit a petition type.
         [HttpPost]
         [Route("Edit")]
         [AllowAnonymous]
-        public async Task<RolResponse> Edit(IdValue request)
+        public async Task<PetitionTypeResponse> Edit(PetitionTypeRequest request)
         {
-            RolResponse response = new RolResponse();
+            PetitionTypeResponse response = new PetitionTypeResponse();
 
             try
             {
@@ -96,12 +93,13 @@ namespace NTTLapso.Controllers
             return response;
         }
 
+        // Delete a petition type
         [HttpGet]
         [Route("Delete")]
         [AllowAnonymous]
-        public async Task<RolResponse> Delete(int Id)
+        public async Task<PetitionTypeResponse> Delete(int Id)
         {
-            RolResponse response = new RolResponse();
+            PetitionTypeResponse response = new PetitionTypeResponse();
 
             try
             {
---
+++ NTTLapso/Repository/PetitionTypeRepository.cs
@@ -0,0 +1,118 @@
+﻿using Dapper;
+using MySqlConnector;
+using NTTLapso.Models.General;
+using NTTLapso.Models.PetitionType;
+using System.Web.WebPages;
+
+namespace NTTLapso.Repository
+{
+    public class PetitionTypeRepository
+    {
+        private static string connectionString = "Server=POAPMYSQL143.dns-servicio.com;User ID=nttlapso;Password=kP0?8u50a;Database=8649628_nttlapso";
+        private MySqlConnection conn;
+
+        public PetitionTypeRepository()
+        {
+            conn = new MySqlConnection(connectionString);
+        }
+
+        // Get petition type list.
+        public async Task<List<PetitionTypeDataResponse>> List(PetitionTypeRequest? request)
+        {
+            List<PetitionTypeDataResponse> response = new List<PetitionTypeDataResponse>();
+            string SQLQueryGeneral = "SELECT Id, Value, Selectable FROM petition_type WHERE 1=1";
+
+            if (request != null && request.Id > 0)
+            {
+                SQLQueryGeneral += " AND Id={0}";
+            }
+            if (request != null && request.Value != null && request.Value != "")
+            {
+                SQLQueryGeneral += " AND Value LIKE '%{1}%'";
+            }
+            if (request != null && request.Selectable != null && request.Selectable == true)
+            {
+                SQLQueryGeneral += " AND Selectable = 1";
+            }
+            else if (request != null && request.Selectable != null && request.Selectable == false)
+            {
+                SQLQueryGeneral += " AND Selectable = 0";
+            }
+
+            string SQLQuery = String.Format(SQLQueryGeneral, request.Id, request.Value);
+            response = (await conn.QueryAsync<PetitionTypeDataResponse>(SQLQuery)).ToList();
+
+            if (response.Count == 0) 
+            {
+                throw new Exception(message: "There are no data in the database or there are none matching the applied filter.");
+            }
+
+            return response;
+        }
+
+        // Create new petition type.
+        public async Task Create(string value, bool selectable)
+        {
+            string sqlSelect = String.Format("SELECT `value` FROM petition_type WHERE LOWER(`value`) = LOWER('{0}');", value);
+            var query = await conn.QueryAsync(sqlSelect); // Checks if the petition type already exists.
+
+            if (query.Count() == 0) // If doesn´t exist we insert petition type.
+            {
+                if (!value.IsEmpty()) // Check if value is empty string.
+                {
+                    string SQLQueryGeneral = String.Format("INSERT INTO petition_type(Value, Selectable) VALUES('{0}', {1})", value, selectable);
+                    await conn.QueryAsync(SQLQueryGeneral);
+                }
+                else
+                {
+                    throw new Exception(message: $"The value can't be empty");
+                }
+            }
+            else
+            {
+                throw new Exception(message: $"The '{value}' type already exists in the database.");
+            }
+        }
+
+        // Edit a petition type.
+        public async Task Edit(PetitionTypeRequest request)
+        {
+            string sqlSelect = String.Format("SELECT `id` FROM petition_type WHERE id = {0};", request.Id);
+            var query = await conn.QueryAsync(sqlSelect); // Checks if the petition type exists.
+
+            if (query.Count() != 0) // If exists we update petition type.
+            {
+                if (!request.Value.IsEmpty() && request.Selectable.HasValue) // Check if value of request is empty string.
+                {
+                    string SQLQueryGeneral = String.Format("UPDATE petition_type SET Value='{1}', Selectable={2} WHERE Id={0}", request.Id, request.Value, request.Selectable);
+                    await conn.QueryAsync(SQLQueryGeneral);
+                }
+                else
+                {
+                    throw new Exception(message: $"The value / selectable fields can't be empty or null");
+                }
+            }
+            else
+            {
+                throw new Exception(message: $"The '{request.Value}' type can't be updated because it doesn't exist in the database.");
+            }
+        }
+
+        // Delete a petition type
+        public async Task Delete(int Id)
+        {
+            string sqlSelect = String.Format("SELECT `id` FROM petition_type WHERE id = {0};", Id);
+            var query = await conn.QueryAsync(sqlSelect); // Checks if the petition type exists.
+
+            if (query.Count() != 0) // If exists we update petition type.
+            {
+                string SQLQueryGeneral = String.Format("DELETE FROM petition_type WHERE Id={0};", Id);
+                await conn.QueryAsync(SQLQueryGeneral);
+            }
+            else
+            {
+                throw new Exception(message: $"The type with id {Id} can't be deleted because it doesn't exist in the database.");
+            }
+        }
+    }
+}
